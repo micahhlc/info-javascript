e0304820703569da94bd15393c2d7e7faf92905e
Micah Cheng <micahhlc@gmail.com>
2025-11-17 21:02:30 +0900

    update string.js with function ucFirst

--- fundamental/string.js
+++ fundamental/string.js
@@ -1,26 +1,20 @@
 let str = '';
 
+// function ucFirst(str) {
+//   if (str) {
+//     let fCCodePoint = str[0].codePointAt(0);
+//     firstChar = String.fromCodePoint(fCCodePoint - 32);
+//     firstChar = fCCodePoint < 90 ? String.fromCodePoint(fCCodePoint + 31) : firstChar;
+
+//     let remainChars;
+//     remainChars = str.substring(1, str.length);
+//     return firstChar + remainChars;
+//   }
+// }
+
 function ucFirst(str) {
-  if (str) {
-    let firstChar = str[0];
-    // console.log(firstChar);
-    let fCCodePoint = firstChar.codePointAt(0);
-    // console.log(fCCodePoint - 32);
-
-    firstChar = String.fromCodePoint(fCCodePoint - 32);
-    firstChar = fCCodePoint < 90 ? String.fromCodePoint(fCCodePoint + 31) : firstChar;
-    // console.log(firstChar);
-
-    let remainChars;
-    remainChars = str.substring(1, str.length);
-    return firstChar + remainChars;
-  }
+  if (!str) return str;
+  return str[0].toUpperCase() + str.slice(1);
 }
 
-// console.log(ucFirst(str));
-
-let charSet = '';
-
-// for (let i = 65; i <= 220; i++) {
-//   console.log(`${i}: ${String.fromCodePoint(i)}`);
-// }
+console.log(ucFirst('AassaAohn')); // John
